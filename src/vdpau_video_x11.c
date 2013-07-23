cf6afbfa34bc942c274571b99b3a4c034fd2c543
Bart Cerneels <bart@theapplitree.be>
2013-07-23 16:49:18 +0200

    remove 1-paint-per-sec

--- src/vdpau_video_x11.c
+++ src/vdpau_video_x11.c
@@ -38,8 +38,6 @@
 #define DEBUG 1
 #include "debug.h"
 
-uint32_t bend_ui = 0;
-
 // Handle VT display preemption
 static int handle_display_preemption(vdpau_driver_data_t *driver_data)
 {
@@ -678,7 +676,7 @@ flip_surface_unlocked(
 
     VdpStatus vdp_status;
 
-    if (driver_data->bahluxid && (bend_ui++%25 ==0)) {
+    if (driver_data->bahluxid) {
         xcb_shm_get_image_cookie_t image_cookie = xcb_shm_get_image(driver_data->xcb_conn,
                                                             driver_data->bahluxid, 0, 0,
                                                             obj_output->width,
