e3b5c93f395be14e8eb0cfc45a5c686da753f83a
Bart Cerneels <bart@theapplitree.be>
2013-10-25 13:24:12 +0200

    Rightbrain build script and submodule for VDPAU.
    submodule points to a specific platform revision.

---
+++ build.sh
@@ -0,0 +1,34 @@
+#!/bin/bash
+
+BUILDDIR=`pwd`
+PREFIX=`pwd`/prefix
+
+if [ ! -e build.env ]; then echo "Build environment not configured, run setup-build.sh"; exit 1; fi
+
+source build.env
+
+mkdir -p $PREFIX/usr/include/vdpau
+mkdir -p $PREFIX/usr/lib
+
+echo "Copying VDPAU headers and libs from viatech-drivers"
+[ -x `which basename` ] || { echo "basename command not found"; exit 1; }
+vdpau_tarball=`ls platform/viatech-drivers/VDPAU_*`
+vdpau_release=`basename $vdpau_tarball | sed s/\.tar\.gz//`
+tar -f $vdpau_tarball -C prefix/usr/include/vdpau --wildcards -x "$vdpau_release/include/*" --strip-components=2
+tar -f $vdpau_tarball -C prefix/usr/lib --wildcards -x "$vdpau_release/bin/*.so*" --strip-components=2
+cd prefix/usr/lib
+ln -s libvdpau.so.* libvdpau.so
+
+cd $BUILDDIR
+# reconfigure using host version. host-autotools break because of hard-coded paths
+PATH=/usr/bin autoreconf -v --install
+CFLAGS="-I$PREFIX/usr/include" LDFLAGS="-L$PREFIX/usr/lib" ./configure --prefix=/usr || exit 1
+make || exit 1
+make install DESTDIR=$PREFIX
+
+cd $BUILDDIR
+mkdir -p target
+#TODO: create rootfs filesystem tarball in target/ (/usr/lib, etc)
+cp $PREFIX/usr/lib/dri/vdpau_drv_video.so target
+echo Build result:
+ls -l  target
